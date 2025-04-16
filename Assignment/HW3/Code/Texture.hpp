7d41912ff86afcaf986bf46f41f2c149e234c0a4
Mes <mes900903@gmail.com>
2025-04-17 00:33:30 +0800

    Update HW3

--- Assignment/HW3/Code/Texture.hpp
+++ Assignment/HW3/Code/Texture.hpp
@@ -4,30 +4,32 @@
 
 #ifndef RASTERIZER_TEXTURE_H
 #define RASTERIZER_TEXTURE_H
-#include "global.hpp"
 #include <Eigen/Eigen>
+#include "global.hpp"
 #include "opencv2/opencv.hpp"
+
 class Texture {
-private:
-  cv::Mat image_data;
+  private:
+	cv::Mat image_data;
 
-public:
-  Texture(const std::string &name)
-  {
-    image_data = cv::imread(name);
-    cv::cvtColor(image_data, image_data, cv::COLOR_RGB2BGR);
-    width = image_data.cols;
-    height = image_data.rows;
-  }
+  public:
+	Texture(std::string const& name) {
+		image_data = cv::imread(name);
+		cv::cvtColor(image_data, image_data, cv::COLOR_RGB2BGR);
+		width = image_data.cols;
+		height = image_data.rows;
+	}
 
-  int width, height;
+	int width, height;
 
-  Eigen::Vector3f getColor(float u, float v)
-  {
-    auto u_img = u * width;
-    auto v_img = (1 - v) * height;
-    auto color = image_data.at<cv::Vec3b>(v_img, u_img);
-    return Eigen::Vector3f(color[0], color[1], color[2]);
-  }
+	Eigen::Vector3f getColor(float u, float v) {
+		u = std::fmin(1, std::fmax(u, 0));
+		v = std::fmin(1, std::fmax(v, 0));
+
+		auto u_img = u * width;
+		auto v_img = (1 - v) * height;
+		auto color = image_data.at<cv::Vec3b>(v_img, u_img);
+		return Eigen::Vector3f(color[0], color[1], color[2]);
+	}
 };
-#endif    // RASTERIZER_TEXTURE_H
+#endif // RASTERIZER_TEXTURE_H
